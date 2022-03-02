c633492dd15f69aa2e191bf3b2f2a0c4d557c3cb
BasaRichard <32295357+BasaRichard@users.noreply.github.com>
2022-03-02 10:33:55 +0100

    Update DummySensor.cs
    DummySensor implemented.

--- src/AutomatedCar/SystemComponents/DummySensor.cs
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -5,8 +5,28 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
 
-    class DummySensor
+    class DummySensor : SystemComponent
     {
+        private DummyPacket dummyPacket;
+
+        public DummySensor(VirtualFunctionBus virtualFunctionBus)
+            : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        public override void Process()
+        {
+            Circle cir = (Circle)World.Instance.WorldObjects.Where(x => x.GetType() == typeof(Circle)).FirstOrDefault();
+
+            AutomatedCar automatedCar = World.Instance.ControlledCar;
+
+            this.dummyPacket.DistanceX = Math.Abs(cir.X - automatedCar.Y);
+            this.dummyPacket.DistanceX = Math.Abs(cir.Y - automatedCar.X);
+        }
     }
 }
